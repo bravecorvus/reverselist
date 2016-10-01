df09e5972ce38e7f17d33c3427d0a516d4747caf
Andrew Lee <leeas@stolaf.edu>
2016-10-01 18:00:06 -0500

    partially finished cpp

---
+++ reverselist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int reverseintlist(int arg) {
+
+}
+
+string reversestringlist(string arg) {
+  
+}
+
+char reversecharlist(char arg) {
+
+}
+
+bool reverseboollist(bool arg) {
+
+}
+
+double reversedoublelist(double arg) {
+  
+}
+
+int main() {
+
+}
